a7cd298c110979c11ebc0132f4c99e60e8c88bed
abir14016 <abir14016@gmail.com>
2022-09-13 01:30:08 +0600

    dashboard designed

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ import Order from './Pages/Order/Order';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 import MedicinalPlants from './Pages/MedicinalPlants/MedicinalPlants';
-import MyOrders from './Pages/MyOrders/MyOrders';
+// import MyOrders from './Pages/MyOrders/MyOrders';
 import AllProducts from './Pages/AllProducts/AllProducts';
 import SingleMedicinalPlant from './Pages/SingleMedicinalPlant/SingleMedicinalPlant';
 import RequireAuth from './Pages/Shared/RequireAuth/RequireAuth';
 import CheckOut from './Pages/CheckOut/CheckOut';
 import { ToastContainer } from "react-toastify";
+import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
+import MyOrders from './Pages/Dashboard/MyOrders/MyOrders';
+import MyReview from './Pages/Dashboard/MyReview/MyReview';
 
 function App() {
   return (
@@ -37,7 +40,13 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
-        <Route path='/myorders' element={<MyOrders></MyOrders>}></Route>
+        {/* <Route path='/myorders' element={<MyOrders></MyOrders>}></Route> */}
+        <Route path='/dashboard' element={<RequireAuth>
+          <Dashboard></Dashboard>
+        </RequireAuth>}>
+          <Route index element={<MyOrders></MyOrders>}></Route>
+          <Route path='myreview' element={<MyReview></MyReview>}></Route>
+        </Route>
       </Routes>
       <Footer></Footer>
       <ToastContainer></ToastContainer>
---
+++ src/Pages/Dashboard/MyReview/MyReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyReview = () => {
+    return (
+        <div>
+            <h4>My review here</h4>
+        </div>
+    );
+};
+
+export default MyReview;
