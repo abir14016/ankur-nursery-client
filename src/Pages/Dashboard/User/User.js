dbdcab4d10779b9fc9d8e11c598e0f1e2d35ac84
abir14016 <abir14016@gmail.com>
2022-09-14 04:00:28 +0600

    all users page design

---
+++ src/Pages/Dashboard/User/User.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const User = ({ user }) => {
+    const { image, name, email, role } = user
+    return (
+        <tr>
+            <td>
+                <div className="avatar">
+                    <div className=" w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={image} alt='user' />
+                    </div>
+                </div>
+            </td>
+            <td>{name}</td>
+            <td>{email}</td>
+            {
+                !role ? <td><button className="btn btn-xs">Make Admin</button></td> : <td>{role}</td>
+            }
+            <td>
+                <button className="btn btn-error btn-circle btn-outline">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </td>
+        </tr>
+    );
+};
+
+export default User;
--- src/Pages/Dashboard/Users/Users.js
+++ src/Pages/Dashboard/Users/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import User from '../User/User';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -12,7 +13,28 @@ const Users = () => {
     }, [])
     return (
         <div>
-            <h2>All users here: {users.length}</h2>
+            <h2 className='mt-5 text-bold text-blue-600 font-bold'>Manage all users</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th className='text-primary'>Image</th>
+                            <th className='text-primary'>Name</th>
+                            <th className='text-primary'>Email</th>
+                            <th className='text-primary'>Role</th>
+                            <th className='text-primary'>Delete User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <User
+                                key={user._id}
+                                user={user}
+                            ></User>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
